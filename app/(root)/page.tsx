8015971f5fd37f0469cb99b21b45ab53e2bbb727
Your Name <severus.key308@gmail.com>
2024-06-07 07:05:33 +0100

    07'05

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -2,14 +2,14 @@
 import { useQuery } from "convex/react";
 import { api } from "@/convex/_generated/api";
 
-import { podcastData } from '@/constants'
+// import { podcastData } from '@/constants'
 import PodcastCard from '@/components/PodcastCard'
 
 
 
 
 const Home = () => {
-  // const tasks = useQuery(api.tasks.get);
+  const trendingPodcasts = useQuery(api.podcasts.getTrendingPodcasts);
 
   return (
     <div className='mt-9 flex flex-col gap-9'>
@@ -17,13 +17,13 @@ const Home = () => {
         <h1 className='text-20 font-bold text-white-1'>Trending Podcasts</h1>
 
         <div className="podcast_grid">
-          {podcastData.map(({ id, title, description, imgURL }) => (
+          {trendingPodcasts?.map(({ _id, podcastTitle, podcastDescription, imageUrl }) => (
             <PodcastCard
-              key={id}
-              imgURL={imgURL as string}
-              title={title}
-              description={description}
-              podcastId={id}
+              key={_id}
+              imgURL={imageUrl as string}
+              title={podcastTitle}
+              description={podcastDescription}
+              podcastId={_id}
             />
           ))}
         </div>
